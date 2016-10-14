53449a7178e28f8c2a0be0692856cd46357a0f65
TATARINOV DMITRY <tito_alba@mail.ru>
2016-10-14 22:52:09 +0300

    Домашнее задание по python, вариант 4

---
+++ HW3/HW3_вар4.py
@@ -0,0 +1,16 @@
+print ('Введите число А')
+a = int(input())
+print ('Введите число B')
+b = int(input())
+print ('Введите число C')
+c = int(input())
+print ('a + b = c?')
+if a+b==c:
+    print ('YES')
+else:
+    print ('NO')
+print ('a/b = c?')
+if a//b==c:
+    print ('YES')
+else:
+    print ('NO')
