ce63067b06e1428ff3e747a7d9b694e513e3f78f
TATARINOV DMITRY <tito_alba@mail.ru>
2017-02-13 23:38:35 +0300

    Домашнее задание по python к 14.02.17, вариант 4

--- HW14/HW14_вар4.py
+++ HW14/HW14_вар4.py
@@ -8,15 +8,14 @@ def read_file():
 
 def substitution ():
     text=read_file()
-    form=re.sub('Философcк', 'Астрологическ', text)
-    form2 = re.sub('философск', 'астрологическ', form)
-    form3=re.sub('философ', 'астролог', form2)
-    form4=re.sub('[Фф][Ии][Лл][Оо][Сс][Оо][Фф]', 'Астролог', form3)
-    form5=re.sub('Филосо́фия', 'Астроло́гия', form4)
-    return form5
+    form=re.sub('философи(я|и|ю|ей|й|ями?|ях)', 'астрологи\\1', text)
+    form2=re.sub('Философи(я|и|ю|ей|й|ями?|ях)', 'Астрологи\\1', form)
+    form3=re.sub('Филосо́фия', 'Астрология', form2)
+    form4=re.sub('ФИЛОСОФИ(И|Я)', 'АСТРОЛОГИ\\1', form3)
+    return form4
 
 def main():
-    new_file=open('Астрология.html','w',encoding='utf-8')
+    new_file=open('Астрология.html', 'w', encoding='utf-8')
     text=new_file.write(substitution())
     new_file.close()
 
